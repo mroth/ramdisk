989a7b2cd5ca0326b46f67ebacccbc491f1de3a4
Matthew Rothenberg <mrothenberg@gmail.com>
2019-01-11 12:21:55 -0500

    platform(linux): implement linux support via tmpfs

---
+++ Makefile
@@ -0,0 +1,15 @@
+.PHONY: test test-docker
+
+test:
+	go test -v
+
+# run the integration tests in a docker container (linux platform target)
+#
+# this requires --privileged execution, but is still significantly faster than
+# firing up a full Linux VM to test if developing on a different platform.
+test-docker:
+	docker run --privileged \
+		-e "GO111MODULE=on" \
+		-v "$(CURDIR):/code" \
+		--workdir "/code" \
+		golang:1.11-alpine "go" "test" "-v"
---
+++ ramdisk_linux.go
@@ -0,0 +1,35 @@
+package ramdisk
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+)
+
+// LinuxPlatformImplmentation is the implementation for Linux systems.
+//
+// The Linux implementation likely *requires sudo* to function on most distros.
+// If you want a sudo-less option, you can simply use /dev/shm instead on most
+// modern Linux platforms.
+type LinuxPlatformImplementation struct{}
+
+func init() {
+	implementation = LinuxPlatformImplementation{}
+}
+
+func (i LinuxPlatformImplementation) create(opts Options) (*RAMDisk, error) {
+	rd := RAMDisk{DevicePath: opts.MountPath, MountPath: opts.MountPath}
+	sizeFlag := fmt.Sprintf("size=%d", opts.Size)
+	cmd := exec.Command(
+		"mount", "-v", "-t", "tmpfs", "-o", sizeFlag, "tmpfs", opts.MountPath)
+	stdout, err := cmd.Output()
+	if err == nil && opts.Logger != nil {
+		opts.Logger.Printf("%s\n", bytes.TrimSpace(stdout))
+	}
+	return &rd, err
+}
+
+func (i LinuxPlatformImplementation) destroy(devicePath string) error {
+	cmd := exec.Command("umount", devicePath)
+	return cmd.Run()
+}
